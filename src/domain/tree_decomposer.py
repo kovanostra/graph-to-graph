fe0b35a902983bf3a6a013b4491e3be8769a6487
kovanostra <kovanostra@gmail.com>
2020-02-09 13:06:32 +0100

    Is able to create a tree when rings share two or more atoms

--- src/domain/tree_decomposer.py
+++ src/domain/tree_decomposer.py
@@ -11,15 +11,17 @@ class TreeDecomposer:
 
     def decompose(self, molecule: rdkit.Chem.rdchem.Mol) -> Graph:
         bonds_not_in_rings = self.extract_all_bonds_not_in_molecular_rings(molecule)
-        rings = self._get_smallest_set_of_smallest_rings(molecule)
+        rings = self._get_smallest_set_of_smallest_rings_merged_if_they_share_more_than_two_atoms(molecule)
         junction_tree = self._create_junction_tree(bonds_not_in_rings, rings)
         return junction_tree
 
-    @staticmethod
-    def _get_smallest_set_of_smallest_rings(molecule: rdkit.Chem.rdchem.Mol) -> rdkit.Chem.GetSymmSSSR:
+    def _get_smallest_set_of_smallest_rings_merged_if_they_share_more_than_two_atoms(self,
+                                                                                     molecule: rdkit.Chem.rdchem.Mol) -> rdkit.Chem.GetSymmSSSR:
         rings = []
         for ring in Chem.GetSymmSSSR(molecule):
             rings.append(list(ring))
+        if len(rings) > 1:
+            rings = self._merge_rings_sharing_more_than_two_atoms(sorted(rings))
         return rings
 
     @staticmethod
@@ -38,7 +40,8 @@ class TreeDecomposer:
             current_cluster = all_clusters[current_cluster_index]
             for next_cluster_index in range(current_cluster_index, tree_nodes_count):
                 next_cluster = all_clusters[next_cluster_index]
-                if current_cluster[-1] == next_cluster[0]:
+                if current_cluster[-1] == next_cluster[0] or self._clusters_share_two_atoms(current_cluster,
+                                                                                            next_cluster):
                     adjacency_matrix[current_cluster_index, next_cluster_index] = 1
                     adjacency_matrix[next_cluster_index, current_cluster_index] = 1
 
@@ -47,3 +50,34 @@ class TreeDecomposer:
     @staticmethod
     def _create_graph(adjacency_matrix: np.array) -> Graph:
         return Graph(adjacency_matrix, np.array([[]]), np.array([[]]))
+
+    def _merge_rings_sharing_more_than_two_atoms(self, rings: list) -> list:
+        merged_rings = []
+        ring_index = 0
+        while ring_index < len(rings) - 1:
+            current_ring = rings[ring_index]
+            next_ring = rings[ring_index + 1]
+            if self._rings_share_more_than_two_atoms(current_ring, next_ring):
+                merged_rings.append(self._get_biggest_ring(current_ring, next_ring))
+                ring_index += 2
+            else:
+                merged_rings.append(current_ring)
+                ring_index += 1
+        if ring_index == 1:
+            merged_rings.append(next_ring)
+        return merged_rings
+
+    @staticmethod
+    def _get_biggest_ring(current_ring: list, next_ring: list) -> list:
+        biggest_ring = current_ring
+        if len(current_ring) < len(next_ring):
+            biggest_ring = next_ring
+        return biggest_ring
+
+    @staticmethod
+    def _rings_share_more_than_two_atoms(current_ring: list, next_ring: list) -> bool:
+        return len(set(current_ring) & set(next_ring)) > 2
+
+    @staticmethod
+    def _clusters_share_two_atoms(current_cluster: list, next_cluster: list) -> bool:
+        return len(current_cluster) > 2 and len(next_cluster) > 2 and len(set(current_cluster) & set(next_cluster)) == 2
--- tests/domain/test_tree_decomposer.py
+++ tests/domain/test_tree_decomposer.py
@@ -11,12 +11,43 @@ class TestTreeDecomposer(TestCase):
     def setUp(self) -> None:
         self.tree_decomposer = TreeDecomposer()
 
-    def test_tree_decomposer_when_one_ring_and_one_bond(self):
+    def test_tree_decomposer_when_molecule_contains_one_bond_and_one_ring(self):
         # Given
-        smiles = 'Cc1ccccc1'
-        molecule = Chem.MolFromSmiles(smiles)
+        molecule_in_smiles = 'Cc1ccccc1'
+        molecule = Chem.MolFromSmiles(molecule_in_smiles)
         junction_tree_expected = Graph(np.array([[0, 1],
                                                  [1, 0]]),
+                                       Chem.GetAdjacencyMatrix(molecule),
+                                       Chem.GetAdjacencyMatrix(molecule))
+
+        # When
+        junction_tree = self.tree_decomposer.decompose(molecule)
+
+        # Then
+        self.assertTrue(np.array_equal(junction_tree_expected.adjacency_matrix, junction_tree.adjacency_matrix))
+
+    def test_tree_decomposer_when_molecule_contains_one_bond_and_two_rings_that_share_more_than_two_atoms(self):
+        # Given
+        molecule_in_smiles = 'Cc1cc2C=Cc(c1)c2'
+        molecule = Chem.MolFromSmiles(molecule_in_smiles)
+        junction_tree_expected = Graph(np.array([[0, 1],
+                                                 [1, 0]]),
+                                       Chem.GetAdjacencyMatrix(molecule),
+                                       Chem.GetAdjacencyMatrix(molecule))
+
+        # When
+        junction_tree = self.tree_decomposer.decompose(molecule)
+
+        # Then
+        self.assertTrue(np.array_equal(junction_tree_expected.adjacency_matrix, junction_tree.adjacency_matrix))
+
+    def test_tree_decomposer_when_molecule_contains_one_bond_and_two_rings_that_share_only_two_atoms(self):
+        # Given
+        molecule_in_smiles = 'CC1=CC2=CC=CC=C2C=C1'
+        molecule = Chem.MolFromSmiles(molecule_in_smiles)
+        junction_tree_expected = Graph(np.array([[0, 1, 0],
+                                                 [1, 0, 1],
+                                                 [0, 1, 0]]),
                                        Chem.GetAdjacencyMatrix(molecule),
                                        Chem.GetAdjacencyMatrix(molecule))
 
