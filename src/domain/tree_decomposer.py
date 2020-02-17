347f7da732b9c25eaec6f3aad75831b219a05dd5
kovanostra <kovanostra@gmail.com>
2020-02-17 20:30:36 +0100

    Refactoring#5 (Replaced variables with attributes)

--- src/domain/tree_decomposer.py
+++ src/domain/tree_decomposer.py
@@ -14,14 +14,16 @@ class TreeDecomposer:
         self.number_of_atoms_in_molecule = None
         self.cluster_graph = None
         self.number_of_nodes_in_cluster_graph = None
+        self.junction_tree = None
+        self.number_of_nodes_in_junction_tree = None
 
     def decompose(self, molecule: rdkit.Chem.rdchem.Mol) -> Graph:
         self._set_molecular_properties(molecule)
         self._extract_all_bonds_not_in_molecular_rings()
         self._merge_cluster_rings_sharing_more_than_two_atoms()
         self._create_cluster_graph()
-        self._get_cluster_graph_without_rings()
-        return self._create_junction_tree()
+        self._create_junction_tree()
+        return self._convert_junction_tree_to_graph()
 
     def _set_molecular_properties(self, molecule: rdkit.Chem.rdchem.Mol) -> None:
         self.molecule = molecule
@@ -52,36 +54,37 @@ class TreeDecomposer:
         self.cluster_graph = sorted(self.bonds + self.rings)
         self.number_of_nodes_in_cluster_graph = len(self.cluster_graph)
 
-    def _get_cluster_graph_without_rings(self) -> None:
+    def _create_junction_tree(self) -> None:
         new_nodes = []
         for atom in range(self.number_of_atoms_in_molecule):
-            ring_candidate = []
-            for cluster_index in range(self.number_of_nodes_in_cluster_graph):
-                if atom in self.cluster_graph[cluster_index]:
-                    ring_candidate.append(cluster_index)
-                    new_nodes = self._break_rings_in_cluster_graph(atom, new_nodes, ring_candidate)
-        self.cluster_graph.extend(new_nodes)
-        self.cluster_graph = sorted(self.cluster_graph)
-        self.number_of_nodes_in_cluster_graph = len(self.cluster_graph)
+            new_nodes = self._detect_and_break_rings_containing_an_atom(atom, new_nodes)
+        self._update_cluster_graph_with_intersecting_nodes(new_nodes)
 
-    def _create_junction_tree(self) -> Graph:
+    def _convert_junction_tree_to_graph(self) -> Graph:
         adjacency_matrix = self._initialize_adjacency_matrix()
-        for current_cluster_index in range(self.number_of_nodes_in_cluster_graph - 1):
-            for next_cluster_index in range(current_cluster_index, self.number_of_nodes_in_cluster_graph):
-                if self._clusters_share_at_least_one_atom(current_cluster_index, next_cluster_index):
+        for current_cluster_index in range(self.number_of_nodes_in_junction_tree - 1):
+            for next_cluster_index in range(current_cluster_index, self.number_of_nodes_in_junction_tree):
+                if self._clusters_share_at_least_one_atom(self.junction_tree, current_cluster_index,
+                                                          next_cluster_index):
                     adjacency_matrix[current_cluster_index, next_cluster_index] = 1
                     adjacency_matrix[next_cluster_index, current_cluster_index] = 1
 
         return self._create_graph(adjacency_matrix)
 
-    def _initialize_adjacency_matrix(self):
-        adjacency_matrix = np.zeros((self.number_of_nodes_in_cluster_graph, self.number_of_nodes_in_cluster_graph))
-        return adjacency_matrix
+    def _detect_and_break_rings_containing_an_atom(self, atom, new_nodes):
+        ring_candidate = []
+        for cluster_index in range(self.number_of_nodes_in_cluster_graph):
+            if atom in self.cluster_graph[cluster_index]:
+                ring_candidate.append(cluster_index)
+                new_nodes = self._break_rings_in_cluster_graph(atom, new_nodes, ring_candidate)
+        return new_nodes
 
-    def _clusters_share_at_least_one_atom(self, current_cluster_index: int, next_cluster_index: int) -> bool:
-        current_cluster, next_cluster = self.cluster_graph[current_cluster_index], self.cluster_graph[
-            next_cluster_index]
-        return current_cluster != next_cluster and len(set(current_cluster) & set(next_cluster)) >= 1
+    def _update_cluster_graph_with_intersecting_nodes(self, new_nodes):
+        self.junction_tree = sorted(self.cluster_graph + new_nodes)
+        self.number_of_nodes_in_junction_tree = len(self.junction_tree)
+
+    def _initialize_adjacency_matrix(self):
+        return np.zeros((self.number_of_nodes_in_junction_tree, self.number_of_nodes_in_junction_tree))
 
     def _break_rings_in_cluster_graph(self, atom: int, new_nodes: list, ring_candidate: list) -> list:
         if len(ring_candidate) > 2:
@@ -91,7 +94,7 @@ class TreeDecomposer:
 
     def _remove_remaining_bonds(self, ring_candidate: list) -> None:
         for current_cluster_index, next_cluster_index in itertools.combinations(ring_candidate, 2):
-            if self._clusters_share_at_least_one_atom(current_cluster_index, next_cluster_index):
+            if self._clusters_share_at_least_one_atom(self.cluster_graph, current_cluster_index, next_cluster_index):
                 shared_atoms = self._get_shared_atoms(current_cluster_index, next_cluster_index)
                 self._remove_shared_atoms(current_cluster_index, next_cluster_index, shared_atoms)
 
@@ -133,3 +136,8 @@ class TreeDecomposer:
     @staticmethod
     def _rings_share_more_than_two_atoms(current_ring: list, next_ring: list) -> bool:
         return len(set(current_ring) & set(next_ring)) > 2
+
+    @staticmethod
+    def _clusters_share_at_least_one_atom(graph: list, current_cluster_index: int, next_cluster_index: int) -> bool:
+        current_cluster, next_cluster = graph[current_cluster_index], graph[next_cluster_index]
+        return current_cluster != next_cluster and len(set(current_cluster) & set(next_cluster)) >= 1
