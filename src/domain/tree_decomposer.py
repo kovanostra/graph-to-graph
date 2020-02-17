0d78f50c6a13fde8ec35d63d06f3fed127b11d29
kovanostra <kovanostra@gmail.com>
2020-02-17 09:04:43 +0100

    When a ring is detected, then it creates a new node and intersects it. Cuts all remaining connections by removing the atoms they contain.

--- src/domain/tree_decomposer.py
+++ src/domain/tree_decomposer.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import itertools
 import numpy as np
 import rdkit
@@ -15,9 +17,8 @@ class TreeDecomposer:
         rings_in_molecular_graph = self._get_smallest_set_of_smallest_rings_merged_if_they_share_more_than_two_atoms(
             molecule)
         all_clusters = sorted(bonds_not_in_rings + rings_in_molecular_graph)
-        rings_in_cluster_graph = self._get_rings_in_cluster_graph(all_clusters, molecule)
-        rings_to_break = self._break_rings_in_cluster_graph(all_clusters, rings_in_cluster_graph)
-        junction_tree = self._create_junction_tree(all_clusters, rings_to_break)
+        all_clusters = self._detect_and_break_rings_in_cluster_graph(all_clusters, molecule)
+        junction_tree = self._create_junction_tree(all_clusters)
         return junction_tree
 
     def _get_smallest_set_of_smallest_rings_merged_if_they_share_more_than_two_atoms(self,
@@ -37,23 +38,16 @@ class TreeDecomposer:
                 bonds_not_in_molecular_rings.append([bond.GetBeginAtomIdx(), bond.GetEndAtomIdx()])
         return bonds_not_in_molecular_rings
 
-    def _create_junction_tree(self, all_clusters: list, rings_to_break: list) -> Graph:
-        print(all_clusters)
-        print(rings_to_break)
+    def _create_junction_tree(self, all_clusters: list) -> Graph:
         tree_nodes_count = len(all_clusters)
         adjacency_matrix = np.zeros((tree_nodes_count, tree_nodes_count))
         for current_cluster_index in range(tree_nodes_count - 1):
             current_cluster = all_clusters[current_cluster_index]
             for next_cluster_index in range(current_cluster_index, tree_nodes_count):
                 next_cluster = all_clusters[next_cluster_index]
-                if next_cluster_index != current_cluster_index:
-                    if (len(current_cluster) == 2 and (current_cluster[-1] in next_cluster
-                                                       or current_cluster[0] in next_cluster)
-                        or self._clusters_share_two_atoms(current_cluster, next_cluster)) and \
-                            ([current_cluster_index, next_cluster_index] not in rings_to_break and
-                             [next_cluster_index, current_cluster_index] not in rings_to_break):
-                        adjacency_matrix[current_cluster_index, next_cluster_index] = 1
-                        adjacency_matrix[next_cluster_index, current_cluster_index] = 1
+                if self._clusters_share_at_least_one_atom(current_cluster, next_cluster):
+                    adjacency_matrix[current_cluster_index, next_cluster_index] = 1
+                    adjacency_matrix[next_cluster_index, current_cluster_index] = 1
 
         return self._create_graph(adjacency_matrix)
 
@@ -96,39 +90,60 @@ class TreeDecomposer:
         return len(current_cluster) > 2 and len(next_cluster) > 2 and len(set(current_cluster) & set(next_cluster)) == 2
 
     @staticmethod
-    def _get_rings_in_cluster_graph(all_clusters: list,
-                                    molecule: rdkit.Chem.rdchem.Mol) -> list:
-        rings = []
+    def _clusters_share_at_least_one_atom(current_cluster: list, next_cluster: list) -> bool:
+        return current_cluster != next_cluster and len(set(current_cluster) & set(next_cluster)) >= 1
+
+    def _detect_and_break_rings_in_cluster_graph(self,
+                                                 all_clusters: List[list],
+                                                 molecule: rdkit.Chem.rdchem.Mol) -> list:
+        atoms_in_molecule = len(Chem.GetAdjacencyMatrix(molecule))
+        new_nodes = []
         for atom in range(len(Chem.GetAdjacencyMatrix(molecule))):
-            ring = []
+            ring_candidate = []
             for cluster_index in range(len(all_clusters)):
                 cluster = all_clusters[cluster_index]
                 if atom in cluster:
-                    ring.append(cluster_index)
-            if len(ring) > 2:
-                rings.append(ring)
-        return rings
+                    ring_candidate.append(cluster_index)
+                    if len(ring_candidate) > 2:
+                        new_nodes, atoms_in_molecule = self._intersect_node(all_clusters,
+                                                                            atom,
+                                                                            atoms_in_molecule,
+                                                                            new_nodes,
+                                                                            ring_candidate)
+                        for current_cluster_index, next_cluster_index in itertools.combinations(ring_candidate, 2):
+                            if self._clusters_share_at_least_one_atom(all_clusters[current_cluster_index],
+                                                                      all_clusters[next_cluster_index]):
+                                shared_atoms = self._get_shared_atoms(all_clusters[current_cluster_index],
+                                                                      all_clusters[next_cluster_index])
+                                all_clusters = self._remove_shared_atoms(all_clusters, current_cluster_index,
+                                                                         next_cluster_index, shared_atoms)
+        all_clusters.extend(new_nodes)
+        return sorted(all_clusters)
 
-    def _break_rings_in_cluster_graph(self, all_clusters: list, rings_in_cluster_graph: list) -> list:
-        rings_to_break = []
-        for ring in rings_in_cluster_graph:
-            ring_members = np.array(all_clusters)[ring]
-            max_number_of_shared_atoms = -1
-            min_number_of_shared_atoms = 10
-            edges_to_break = []
-            for pair in itertools.combinations(ring_members, 2):
-                shared_atoms_of_pair = self._number_of_shared_atoms(pair[0], pair[1])
-                pair_with_shared_atoms = list(pair)
-                pair_with_shared_atoms.append([shared_atoms_of_pair])
-                edges_to_break.append(pair_with_shared_atoms)
-                if shared_atoms_of_pair > max_number_of_shared_atoms:
-                    max_number_of_shared_atoms = shared_atoms_of_pair
-                if shared_atoms_of_pair < min_number_of_shared_atoms:
-                    min_number_of_shared_atoms = shared_atoms_of_pair
-            edges_to_break = sorted(edges_to_break, key=lambda x: x[2], reverse=True)
-            if max_number_of_shared_atoms - min_number_of_shared_atoms > 0:
-                pair_cluster_index = [index for index in range(len(all_clusters)) if
-                                      all_clusters[index] == edges_to_break[-1][0] or all_clusters[index] ==
-                                      edges_to_break[-1][1]]
-                rings_to_break.append(pair_cluster_index)
-        return rings_to_break
+    def _intersect_node(self, all_clusters, atom, atoms_in_molecule, new_nodes, ring_candidate):
+        new_atoms_in_molecule = atoms_in_molecule + len(ring_candidate)
+        intersecting_node = self._create_node_with_new_atoms(atoms_in_molecule, new_atoms_in_molecule)
+        for ring_member_index in range(len(intersecting_node)):
+            node_to_modify = ring_candidate[ring_member_index]
+            all_clusters[node_to_modify].remove(atom)
+            all_clusters[node_to_modify].append(intersecting_node[ring_member_index])
+        new_nodes.append(intersecting_node)
+        atoms_in_molecule = new_atoms_in_molecule
+        return new_nodes, atoms_in_molecule
+
+    @staticmethod
+    def _create_node_with_new_atoms(atoms_in_molecule, new_atoms_in_molecule):
+        return [dummy_atom for dummy_atom in
+                range(atoms_in_molecule, new_atoms_in_molecule)]
+
+    @staticmethod
+    def _get_shared_atoms(current_cluster: list, next_cluster: list) -> list:
+        return [atom for atom in current_cluster if atom in next_cluster]
+
+    @staticmethod
+    def _remove_shared_atoms(all_clusters: list, current_cluster_index: int, next_cluster_index: int,
+                             shared_atoms: list) -> list:
+        for atom in shared_atoms:
+            all_clusters[current_cluster_index].remove(atom)
+            all_clusters[next_cluster_index].remove(atom)
+        return all_clusters
